d56490f837e6b7242309789600cd1dea7b2df048
Ryohei Ikegami <iofg2100@gmail.com>
2015-09-02 20:18:32 +0900

    Add implementation and example

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var EVENTS = ["animationstart", "animationiteration", "animationend", "transitionend"];
+var arrayForEach = Array.prototype.forEach;
+
+function AnimationCop() {
+  var self = this;
+  this.onEvent = function (e) {
+    self.animatedElems.add(e.target);
+  };
+}
+
+AnimationCop.prototype.begin = function () {
+  var elems = this.elems = document.querySelectorAll("*");
+  this.animatedElems = new Set();
+  var self = this;
+  arrayForEach.call(elems, function (elem) {
+    EVENTS.forEach(function (event) {
+      elem.addEventListener(event, self.onEvent);
+    });
+  });
+};
+
+AnimationCop.prototype.end = function () {
+  var self = this;
+  arrayForEach.call(this.elems, function (elem) {
+    EVENTS.forEach(function (event) {
+      elem.removeEventListener(event, self.onEvent);
+    });
+  });
+  this.elems = [];
+};
+
+AnimationCop.prototype.watchFor = function (ms) {
+  var self = this;
+  this.begin();
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      self.end();
+      resolve();
+    }, ms);
+  });
+};
